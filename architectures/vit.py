46dea5ef556e2c512e9c0a54bbebcc412eb56c27
Mahdi Salmani <m10.salmani@gmail.com>
2023-07-04 19:06:04 +0330

    change freeze

--- architectures/vit.py
+++ architectures/vit.py
@@ -391,15 +391,15 @@ class VisionTransformer(nn.Module):
         return x
 
     
-    def freeze_except(self, end=None):
+    def freeze_except(self, start=0, end=None):
         if end is None:
             end = self.depth
-        if end == self.depth:
-            self.head.requires_grad_(True)
-        else:
-            self.head.requires_grad_(False)
+        # if end == self.depth:
+        #     self.head.requires_grad_(True)
+        # else:
+        #     self.head.requires_grad_(False)
         for i, blk in enumerate(self.blocks):
-            if i < end:
+            if start <= i < end:
                 blk.requires_grad_(True)
             else:
                 blk.requires_grad_(False)
--- train.py
+++ train.py
@@ -256,9 +256,10 @@ def main():
             if args.method == 'blacksmith':
                 p = 1 if np.random.random() > rate else 0
                 end = args.vit_depth if p == 1 else int(rate * args.vit_depth)
+                start = 0 if p == 0 else int(rate * args.vit_depth)
                 steps = 1 if p == 1 else 2
 
-                model.freeze_except(end)
+                model.freeze_except(start=start, end=end)
                 for j in range(steps):
                     eta.requires_grad = True
                     output = model(X + eta, end=end)
