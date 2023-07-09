30da04c49c8a29b89f4185493a33073772500a61
Mahdi Salmani <m10.salmani@gmail.com>
2023-07-09 11:29:27 +0330

    add clip in blacksmith

--- train.py
+++ train.py
@@ -251,15 +251,18 @@ def main():
                 p = 1 if np.random.random() > rate else 0
                 end = args.vit_depth if p == 1 else round(rate * args.vit_depth)
                 start = 0 if p == 0 else int(rate * args.vit_depth)
-                steps = 2
+                steps = 1 if p == 1 else 2
                 model.freeze_except(start=start, end=end)
 
                 for j in range(steps):
                     eta.requires_grad = True
-                    output = model(X + eta, start=start, end=end)
+                    output = model(X + eta, end=end)
                     loss = F.cross_entropy(output, y)
                     grad = torch.autograd.grad(loss, eta)[0].detach()
-                    delta = attack_utils.clamp(eta + (alpha / steps) * torch.sign(grad), -epsilon, epsilon)
+                    if args.clip > 0:
+                        delta = attack_utils.clamp(eta + (alpha / steps) * torch.sign(grad), -epsilon, epsilon)
+                    else:
+                        eta.data = eta + (alpha / steps) * torch.sign(grad)
                     delta = attack_utils.clamp(delta, attack_utils.lower_limit - X, attack_utils.upper_limit - X)
                     eta = delta.detach()
                 
