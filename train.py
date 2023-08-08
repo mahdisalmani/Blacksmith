756ae84e5782d1d176375f84ee437dd4c7d8e70f
Mahdi Salmani <m10.salmani@gmail.com>
2023-08-08 14:14:55 +0330

    .

--- train.py
+++ train.py
@@ -419,8 +419,14 @@ def main():
             
             middle_X1 = []
             middle_X2 = []
-            output_heat = model(X + eta, end=end, y=middle_X1)
-            output_forge = model(X + eta, y=middle_X2)
+            eta = new_eta.clone().detach()
+            eta.requires_grad = True
+            output = model(X + eta, end=end)
+            loss = F.cross_entropy(output, y)
+            grad = torch.autograd.grad(loss, eta)[0].detach()
+            output_adversary = model(X + attack_utils.clamp(eta + alpha * torch.sign(grad), -epsilon, epsilon), end=end, y=middle_X1)
+            output_normal = model(X, y=middle_X2)
+
             print("L2_DIFF: ", torch.norm((middle_X1[0]-middle_X2[0]).reshape((X.shape[0], -1)), dim=1).mean())
             print("Linf_DIFF: ", torch.norm((middle_X1[0]-middle_X2[0]).reshape((X.shape[0], -1)), dim=1).mean())
             break
