ca4d9c105796f6ecd8b6afb3df0d72a4698a7a0a
alireza <ali.d.f.2012@gmail.com>
2023-08-12 15:46:59 +0800

    debug

--- aa_eval.py
+++ aa_eval.py
@@ -96,7 +96,7 @@ if __name__ == '__main__':
     test_loss, test_acc = attack_utils.evaluate_standard(test_loader, my_model)
     print("test acc: ", test_acc)
 
-    open('./Logs/attack_logs.out', 'a').close()
+    open('./Logs/attack_logs.out', 'w').close()
     adversary = AutoAttack(my_model, norm='Linf', eps=args.epsilon, version='standard',
                            log_path="./Logs/attack_logs.out")
     adversary.seed = 0
